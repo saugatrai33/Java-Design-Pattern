a77dfb8b4df06366bdc76cc8208f5cb478cefc12
Saugat Rai <saugatrai33@gmail.com>
2022-02-19 11:21:02 +0545

    factory pattern

---
+++ src/com/company/factorypattern/ShapeMain.java
@@ -0,0 +1,21 @@
+package com.company.factorypattern;
+
+import com.company.factorypattern.shape.ShapeFactory;
+
+public class ShapeMain {
+    public static void main(String[] args) {
+        ShapeFactory factory = new ShapeFactory();
+
+        var circle = factory.getShape("Circle");
+        circle.draw();
+
+        var triangle = factory.getShape("Triangle");
+        triangle.draw();
+
+        var square = factory.getShape("Square");
+        square.draw();
+
+        var rectangle = factory.getShape("Recatangle");
+        rectangle.draw();
+    }
+}
---
+++ src/com/company/factorypattern/shape/Shape.java
@@ -0,0 +1,5 @@
+package com.company.factorypattern.shape;
+
+public interface Shape {
+    void draw();
+}
